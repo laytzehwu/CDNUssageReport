fa5f8ec6b6ff2e5783379b59ec644192a771f05e
laytze <tzeh.wu.lay@f-secure.com>
2015-04-22 06:27:43 -0700

    Grid

--- spec/CDNUssageViewSpec.js
+++ spec/CDNUssageViewSpec.js
@@ -1,11 +1,75 @@
-describe("CDNUssageView Spec", function () {
-	it("Render ussage row properly", function () {
-		CDNUssageReport.Views.UssageView.renderRow();
-	})
-	it("Render ussage detail", function () {
-		CDNUssageReport.Views.UssageView.renderDetailDialog();
-	})
-	it("Render ussage list", function () {
-		CDNUssageReport.Views.UssageView.renderList();
-	})
+describe("UssageView Spec", function () {
+	var UseCaseModel = CDNUssageReport.Models.UseCaseModel;
+	it("Render full columns header", function () {
+		var $row = CDNUssageReport.Views.UssageView.renderGridHeader();
+		expect($row.hasClass("grid-header")).toBeTruthy();
+		var $cells = $row.find(".grid-cell");
+		var labels = CDNUssageReport.Views.UssageView.getLabels();
+		var labelKeys = Object.keys(labels);
+		expect($cells.length).toEqual(labelKeys.length);
+
+		for(var i=0;i<labelKeys.length;i++) {
+			var $cell = $($cells[i]);
+	 		expect($cell.text()).toEqual(labels[labelKeys[i]]);		
+		}
+
+	});
+
+	it("Render custom columns header", function () {
+		// Only show 1 column
+		var $row = CDNUssageReport.Views.UssageView.renderGridHeader({columns: {publisher: true}});
+		expect($row.hasClass("grid-header")).toBeTruthy();
+		var $cells = $row.find(".grid-cell");
+		var labels = CDNUssageReport.Views.UssageView.getLabels();
+		var labelKeys = Object.keys(labels);
+		expect($cells.length).toEqual(1);
+		expect($cells.text()).toEqual(labels.publisher);
+
+		// Show 2 columns
+		var $row = CDNUssageReport.Views.UssageView.renderGridHeader({columns: {publisher: true, bytesCached: true, requestCached: true}});
+		expect($row.hasClass("grid-header")).toBeTruthy();
+		var $cells = $row.find(".grid-cell");
+		var labels = CDNUssageReport.Views.UssageView.getLabels();
+		var labelKeys = Object.keys(labels);
+		expect($cells.length).toEqual(3);
+		expect($cells.text()).toEqual(labels.publisher+labels.bytesCached+labels.requestCached);
+
+	});
+
+	it("Render ussage row by single use case properly", function () {
+		var useCase = new UseCaseModel([1,1,1,50,50,50,50]);
+		var $row = CDNUssageReport.Views.UssageView.renderRow(useCase);
+		expect($row.hasClass("grid-row")).toBeTruthy();
+		expect($row.find(".grid-cell").length).toEqual(5);
+
+	});
+
+	it("Render ussage full detail", function () {
+		var useCase = new UseCaseModel([1,1,1,50,50,50,50]);
+		var $table = CDNUssageReport.Views.UssageView.renderDetailDialog(useCase);
+		expect($table.hasClass("grid")).toBeTruthy();
+		var labels = CDNUssageReport.Views.UssageView.getLabels();
+		var labelKeys = Object.keys(labels);
+		var $rows = $table.find(".grid-row");
+		expect($rows.length).toEqual(labelKeys.length);
+		var detailHTML = $table.html();
+		for(var i=0;i<labelKeys.length;i++) {
+			var label = labels[labelKeys[i]]
+	 		expect(detailHTML).toMatch(label);		
+		}
+	});
+
+	it("Render ussage list with full columns", function () {
+		var cases = [
+			new UseCaseModel([1,1,1,50,50,50,50]),
+			new UseCaseModel([1,2,1,50,50,50,50]),
+			new UseCaseModel([1,3,1,50,50,50,50]),
+		];
+		var $list = CDNUssageReport.Views.UssageView.renderList(cases);
+		expect($list.hasClass("grid")).toBeTruthy();
+		var $header = $list.find(".grid-header");
+		expect($header.length).toEqual(1);
+		var $header = $list.find(".grid-row");
+		expect($header.length).toEqual(cases.length);
+	});
 });
--- src/CDNUssageView.js
+++ src/CDNUssageView.js
@@ -5,15 +5,170 @@
 var CDNUssageReport = CDNUssageReport || {};
 CDNUssageReport.Views = CDNUssageReport.Views || {};
 CDNUssageReport.Views.UssageView = (function () {
+	var defaultColumnsSetting = {	resource: true,
+					publisher: true,
+					edge: true,
+					bytesCached: true,
+					requestCached: true
+				};
+	var labels = {
+		resource: "Resource",
+		publisher: "Website",
+		edge: "Edge Server",
+		bytesCached: "Bytes Cached",
+		requestCached: "Request Cached"
+	}
 	return {
-		renderRow: function () {
-			throw new Error("Not yet impletemented!");
+		getLabels: function () {
+			return labels;
 		},
-		renderDetailDialog: function () {
-			throw new Error("Not yet impletemented!");
+		renderGridHeader: function (option) {
+			var option = option || {};
+			if(!option.columns) {
+				option.columns = defaultColumnsSetting;
+			}
+			var $row = $("<div class='grid-header'></div>");
+			if(option.columns.resource) {
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(labels.resource);
+				$row.append($cell);
+			}
+			if(option.columns.publisher) {
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(labels.publisher);
+				$row.append($cell);
+			}
+			if(option.columns.edge) {
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(labels.edge);
+				$row.append($cell);
+			}
+			if(option.columns.bytesCached) {
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(labels.bytesCached);
+				$row.append($cell);
+			}
+			if(option.columns.requestCached) {
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(labels.requestCached);
+				$row.append($cell);
+			}
+			return $row;
 		},
-		renderList: function () {
-			throw new Error("Not yet impletemented!");
+		renderRow: function (useCase, option) {
+			var option = option || {};
+			if(!option.columns) {
+				option.columns = defaultColumnsSetting;
+			}
+			var $row = $("<div class='grid-row'></div>");
+			if(option.columns.resource) {
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(useCase.resourceId);
+				$row.append($cell);
+			}
+			if(option.columns.publisher) {
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(useCase.publisherId);
+				$row.append($cell);
+			}
+			if(option.columns.edge) {
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(useCase.edgeId);
+				$row.append($cell);
+			}
+			if(option.columns.bytesCached) {
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(useCase.getCachedPercentage() + "%");
+				$row.append($cell);
+			}
+			if(option.columns.requestCached) {
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(useCase.getRequestCachePercentage() + "%");
+				$row.append($cell);
+			}
+
+			return $row;
+		},
+		renderDetailDialog: function (useCase, option) {
+			var option = option || {};
+			if(!option.columns) {
+				option.columns = defaultColumnsSetting;
+			}
+			var $table = $("<div class='grid'></div>");
+			if(option.columns.resource) {
+				var $row = $("<div class='grid-row'></div>");
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(labels.resource);
+				$row.append($cell);
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(useCase.resourceId);
+				$row.append($cell);
+				$table.append($row);
+			}
+
+			if(option.columns.publisher) {
+				var $row = $("<div class='grid-row'></div>");
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(labels.publisher);
+				$row.append($cell);
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(useCase.publisherId);
+				$row.append($cell);
+				$table.append($row);
+			}
+
+			if(option.columns.edge) {
+				var $row = $("<div class='grid-row'></div>");
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(labels.edge);
+				$row.append($cell);
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(useCase.edgeId);
+				$row.append($cell);
+				$table.append($row);
+			}
+
+			if(option.columns.bytesCached) {
+				var $row = $("<div class='grid-row'></div>");
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(labels.bytesCached);
+				$row.append($cell);
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(useCase.getCachedPercentage() + "%");
+				$row.append($cell);
+				$table.append($row);
+			}
+
+			if(option.columns.requestCached) {
+				var $row = $("<div class='grid-row'></div>");
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(labels.requestCached);
+				$row.append($cell);
+				var $cell = $("<div class='grid-cell'></div>");
+				$cell.text(useCase.getRequestCachePercentage() + "%");
+				$row.append($cell);
+				$table.append($row);
+			}
+
+			return $table;
+		},
+		renderList: function (useCases, option) {
+
+			var option = option || {};
+			if(!option.columns) {
+				option.columns = defaultColumnsSetting;
+			}
+
+			var $grid = $("<div class='grid'></div>");
+			var $header = this.renderGridHeader(option);
+			$grid.append($header);
+			
+			var iLen = useCases.length;
+			for(var i=0;i<iLen;i++) {
+				var $row = this.renderRow(useCases[i]);
+				$grid.append($row);
+			}
+			return $grid;
 		}
 	};
 })();
