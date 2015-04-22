8964991490579a28eb18908def1b4817a2c3a579
laytze <tzeh.wu.lay@f-secure.com>
2015-04-22 08:02:24 -0700

    Statistic in models

--- spec/CDNUssageModelSpec.js
+++ spec/CDNUssageModelSpec.js
@@ -1,35 +1,36 @@
 describe("CDNUssageModel Spec", function () {
+    var UseCaseModel = CDNUssageReport.Models.UseCaseModel; 
     it("Create CDNUssageModel without data", function () {
         expect(function () {
-            new CDNUssageReport.Models.UseCaseModel()
+            new UseCaseModel()
         }).toThrowError("No/Empty data passed in!");
     });
 
     it("Create CDNUssageModel with short of data", function () {
         expect(function () {
-            new CDNUssageReport.Models.UseCaseModel(1)
+            new UseCaseModel(1)
         }).toThrowError("Only accept array!");
 
         expect(function () {
-            new CDNUssageReport.Models.UseCaseModel("ABC")
+            new UseCaseModel("ABC")
         }).toThrowError("Only accept array!");
 
         expect(function () {
-            new CDNUssageReport.Models.UseCaseModel({"id": 1})
+            new UseCaseModel({"id": 1})
         }).toThrowError("Only accept array!");
 
         expect(function () {
-            new CDNUssageReport.Models.UseCaseModel([1,2,4])
+            new UseCaseModel([1,2,4])
         }).toThrowError("Only accept 7 element array!");
 
         expect(function () {
-            new CDNUssageReport.Models.UseCaseModel([1,2,3,4,5,"A",7])
+            new UseCaseModel([1,2,3,4,5,"A",7])
         }).toThrowError("Found pass-in data(5) is not integer!");
         
     });
 
     it("Create CDNUssageModel with proper data", function () {
-	var model = new CDNUssageReport.Models.UseCaseModel([63789283, 957505314, 7690037, 1501776752, 612004274, 5783, 2711]);
+	var model = new UseCaseModel([63789283, 957505314, 7690037, 1501776752, 612004274, 5783, 2711]);
 	expect(model.resourceId).toEqual(63789283);
 	expect(model.publisherId).toEqual(957505314);
 	expect(model.edgeId).toEqual(7690037);
@@ -48,7 +49,7 @@ describe("CDNUssageModel Spec", function () {
 	var requestsCached = 5783;
 	var requestsNotCached = 2711;
 	var rowArray = [resourceId, publisherId, edgeId, bytesCached, bytesNotCached, requestsCached, requestsNotCached];
-	var model = new CDNUssageReport.Models.UseCaseModel(rowArray);
+	var model = new UseCaseModel(rowArray);
 	expect(model.getCachedPercentage()).toEqual(model.bytesCached / (model.bytesCached + model.bytesNotCached) * 100);
 	expect(model.isFullyCached()).toBeFalsy();
 	model.bytesNotCached = 0;
@@ -66,13 +67,134 @@ describe("CDNUssageModel Spec", function () {
 	var requestsCached = 5783;
 	var requestsNotCached = 2711;
 	var rowArray = [resourceId, publisherId, edgeId, bytesCached, bytesNotCached, requestsCached, requestsNotCached];
-	var model = new CDNUssageReport.Models.UseCaseModel(rowArray);
+	var model = new UseCaseModel(rowArray);
 	expect(model.getRequestCachePercentage()).toEqual(model.requestsCached / (model.requestsCached + model.requestsNotCached) * 100);
 	expect(model.isRequestFullyCached()).toBeFalsy();
 	model.requestsNotCached = 0;
 	expect(model.isRequestFullyCached()).toBeTruthy();
 	model.requestsCached = 0;
 	expect(model.isRequestFullyCached()).toBeTruthy();
+    });
+
+});
+
+describe("Statistic spec", function () {
+    var Statistic = CDNUssageReport.Models.Statistic; 
+    it("Pass in nothing is allowed!", function () {
+	var sample = new Statistic();
+	expect(sample.resourceId).toBeNull();	
+	expect(sample.publisherId).toBeNull();	
+	expect(sample.edgeId).toBeNull();	
+
+	expect(sample.bytesCached).toEqual(0);	
+	expect(sample.bytesNotCached).toEqual(0);	
+	expect(sample.requestsCached).toEqual(0);	
+	expect(sample.requestsNotCached).toEqual(0);	
+	expect(sample.length).toEqual(0);	
+    });
+
+    it("Pass in only 1 use case", function () {
+        var useCase = new CDNUssageReport.Models.UseCaseModel([1,2,3,4,5,6,7]);
+	var sample = new Statistic([useCase]);
+	
+	expect(sample.resourceId).toEqual(useCase.resourceId);
+	expect(sample.publisherId).toEqual(useCase.publisherId);
+	expect(sample.edgeId).toEqual(useCase.edgeId);
+
+	expect(sample.bytesCached).toEqual(useCase.bytesCached);
+	expect(sample.bytesNotCached).toEqual(useCase.bytesNotCached);
+	expect(sample.requestsNotCached).toEqual(useCase.requestsNotCached);
+	expect(sample.requestsCached).toEqual(useCase.requestsCached);
+	
+    });
+
+    var getRandomInt = function (min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+    it("Some use cases", function () {
+        var iLen = getRandomInt(1, 20);
+        var useCases = [];
+        var bytesCached = 0;
+        var bytesNotCached = 0;
+        var requestsNotCached = 0;
+        var requestsCached = 0;
+        for(var i=0;i<iLen;i++) {
+	    var resourceId = getRandomInt(1000,9000);
+	    var publisherId = getRandomInt(1000,9000);
+	    var edgeId = getRandomInt(1000,9000);
+            var useCase = new CDNUssageReport.Models.UseCaseModel([resourceId, publisherId, edgeId, getRandomInt(0,100), getRandomInt(0,100), getRandomInt(0,100), getRandomInt(0,100)]);
+            bytesCached += useCase.bytesCached;
+            bytesNotCached += useCase.bytesNotCached;
+            requestsNotCached += useCase.requestsNotCached;
+            requestsCached += useCase.requestsCached;
+            useCases.push(useCase);
+        }
+        var sample = new Statistic(useCases);
+        expect(sample.length).toEqual(iLen);
+        if(sample.bytesCached != bytesCached) {
+            fail("bytesCached calculation fail");
+        }
+        if(sample.bytesNotCached != bytesNotCached) {
+            fail("bytesNotCached calculation fail");
+        }
+        if(sample.requestsNotCached != requestsNotCached) {
+            fail("requestsNotCached calculation fail");
+        }
+        if(sample.requestsCached != requestsCached) {
+            fail("requestsCached calculation fail");
+        }
+       
+    });
+
+    it("Same resource cases",function () {
+        var iLen = getRandomInt(1, 20);
+        var useCases = [];
+        var resourceId = getRandomInt(1000,9000);
+        for(var i=0;i<iLen;i++) {
+	    var publisherId = getRandomInt(1000,9000);
+	    var edgeId = getRandomInt(1000,9000);
+            var useCase = new CDNUssageReport.Models.UseCaseModel([resourceId, publisherId, edgeId, getRandomInt(0,100), getRandomInt(0,100), getRandomInt(0,100), getRandomInt(0,100)]);
+            useCases.push(useCase);
+        }        
+        var sample = new Statistic(useCases);
+        expect(sample.length).toEqual(iLen);
+        expect(sample.resourceId).toEqual(resourceId);
+        expect(sample.publisherId).toBeNull();
+        expect(sample.edgeId).toBeNull();
+    });
+
+    it("Same publisher cases",function () {
+        var iLen = getRandomInt(1, 20);
+        var useCases = [];
+        var publisherId = getRandomInt(1000,9000);
+        for(var i=0;i<iLen;i++) {
+	    var resourceId = getRandomInt(1000,9000);
+	    var edgeId = getRandomInt(1000,9000);
+            var useCase = new CDNUssageReport.Models.UseCaseModel([resourceId, publisherId, edgeId, getRandomInt(0,100), getRandomInt(0,100), getRandomInt(0,100), getRandomInt(0,100)]);
+            useCases.push(useCase);
+        }        
+        var sample = new Statistic(useCases);
+        expect(sample.length).toEqual(iLen);
+        expect(sample.publisherId).toEqual(publisherId);
+        expect(sample.resourceId).toBeNull();
+        expect(sample.edgeId).toBeNull();
+    });
+
+    it("Same edge cases",function () {
+        var iLen = getRandomInt(1, 20);
+        var useCases = [];
+        var edgeId = getRandomInt(1000,9000);
+        for(var i=0;i<iLen;i++) {
+	    var resourceId = getRandomInt(1000,9000);
+	    var publisherId = getRandomInt(1000,9000);
+            var useCase = new CDNUssageReport.Models.UseCaseModel([resourceId, publisherId, edgeId, getRandomInt(0,100), getRandomInt(0,100), getRandomInt(0,100), getRandomInt(0,100)]);
+            useCases.push(useCase);
+        }        
+        var sample = new Statistic(useCases);
+        expect(sample.length).toEqual(iLen);
+        expect(sample.edgeId).toEqual(edgeId);
+        expect(sample.resourceId).toBeNull();
+        expect(sample.publisherId).toBeNull();
     });
 
 });
--- src/CDNUssageModel.js
+++ src/CDNUssageModel.js
@@ -7,6 +7,29 @@
 */
 var CDNUssageReport = CDNUssageReport || {};
 CDNUssageReport.Models = CDNUssageReport.Models || {};
+CDNUssageReport.Models.Abstract = CDNUssageReport.Models.Abstract || {};
+CDNUssageReport.Models.Abstract.getCachedPercentage = function () {
+    if((this.bytesCached === 0) && (this.bytesNotCached)) {
+	return 100;
+    }
+    return this.bytesCached / (this.bytesCached + this.bytesNotCached) * 100;
+}
+
+CDNUssageReport.Models.Abstract.isFullyCached = function () {
+    return !(this.bytesNotCached > 0);
+}
+
+CDNUssageReport.Models.Abstract.getRequestCachePercentage = function () {
+    if((this.requestsCached === 0) && (this.requestsNotCached)) {
+	return 100;
+    }
+    return this.requestsCached / (this.requestsCached + this.requestsNotCached) * 100;
+}
+
+CDNUssageReport.Models.Abstract.isRequestFullyCached = function () {
+    return !(this.requestsNotCached > 0);
+}
+
 CDNUssageReport.Models.UseCaseModel = function (data) {
     if(!data) {
         throw new Error("No/Empty data passed in!");
@@ -38,25 +61,80 @@ CDNUssageReport.Models.UseCaseModel = function (data) {
     this.requestsCached = parseInt(data[5]);
     this.requestsNotCached = parseInt(data[6]); 
 
-    this.getCachedPercentage = function () {
-	if((this.bytesCached === 0) && (this.bytesNotCached)) {
-		return 100;
-	}
-	return this.bytesCached / (this.bytesCached + this.bytesNotCached) * 100;
+    this.getCachedPercentage = CDNUssageReport.Models.Abstract.getCachedPercentage;
+
+    this.isFullyCached = CDNUssageReport.Models.Abstract.isFullyCached;
+
+   this.getRequestCachePercentage = CDNUssageReport.Models.Abstract.getRequestCachePercentage;
+
+   this.isRequestFullyCached = CDNUssageReport.Models.Abstract.isRequestFullyCached;
+}
+
+CDNUssageReport.Models.Statistic = function (list) {
+    this.resourceId = null;
+    this.publisherId = null;
+    this.edgeId = null;
+
+    this.bytesCached = 0;
+    this.bytesNotCached = 0;
+    this.requestsCached = 0;
+    this.requestsNotCached = 0; 
+
+    this.list = [];
+
+    if(list && !Array.isArray(list)) {
+        throw new Error("Please pass-in an array");
     }
 
-    this.isFullyCached = function () {
-	return !(this.bytesNotCached > 0);
+    var list = list || [];
+
+    this.length = list.length;
+    var resourceId = null;
+    var publisherId = null;
+    var edgeId = null;
+
+    for(var i=0;i<this.length; i++) {
+	var useCase = list[i];
+	if(resourceId === null) {
+            resourceId = useCase.resourceId;
+	} else if(resourceId > 0) {
+		if(resourceId != useCase.resourceId) {
+			resourceId = -1;
+		}
+	}
+
+	if(publisherId === null) {
+            publisherId = useCase.publisherId;
+	} else if(publisherId > 0) {
+		if(publisherId != useCase.publisherId) {
+			publisherId = -1;
+		}
+	}
+
+	if(edgeId === null) {
+            edgeId = useCase.edgeId;
+	} else if(edgeId > 0) {
+		if(edgeId != useCase.edgeId) {
+			edgeId = -1;
+		}
+	}
+
+	this.bytesCached += useCase.bytesCached;
+	this.bytesNotCached += useCase.bytesNotCached;
+	this.requestsCached += useCase.requestsCached;
+	this.requestsNotCached += useCase.requestsNotCached; 
+
+	this.list.push(useCase);
+    }
+    
+    if(resourceId > 0) {
+        this.resourceId = resourceId;
+    }
+    if(publisherId > 0) {
+        this.publisherId = publisherId;
+    }
+    if(edgeId > 0) {
+        this.edgeId = edgeId;
     }
 
-   this.getRequestCachePercentage = function () {
-	if((this.requestsCached === 0) && (this.requestsNotCached)) {
-		return 100;
-	}
-	return this.requestsCached / (this.requestsCached + this.requestsNotCached) * 100;
-   }
-
-   this.isRequestFullyCached = function () {
-	return !(this.requestsNotCached > 0);
-   }
 }
